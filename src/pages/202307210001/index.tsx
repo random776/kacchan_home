656a534d6417c4e2c064947c6288ed89ba6d44f9
random776 <kazuki.tdn@gmail.com>
2023-07-22 00:03:11 +0900

    fix news format

--- src/pages/202307210001/index.tsx
+++ src/pages/202307210001/index.tsx
@@ -12,9 +12,15 @@ export default function News() {
             <div><img className="img" src="https://github.com/random776/kacchan-uts2-22/blob/master/pictures/saboro.jpg?raw=true" alt="" /></div>
             <p>テスト期間中につき、以下の日程にて（緊急時を除き）一切のツイートを一時停止し、ログアウトします。</p>
             <p>なお、<a href="https://github.com/random776">GitHub</a>は引き続き稼働します。</p>
-            <h2 className="color4">第3回ツイート一時停止期間</h2>
-            <p>2023年 7/19（水）〜 7/25（火）</p>
-            <p>2023年 7/29（土）〜 8/1（火）</p>
+            <h2 className="color2">第3回ツイート一時停止期間</h2>
+            <ul>
+                <li>
+                2023年 7/19（水）〜 7/25（火）
+                </li>
+                <li>
+                2023年 7/29（土）〜 8/1（火） ※ 予定
+                </li>
+            </ul>
     </div>
         <SubMenu2 />
         </>
