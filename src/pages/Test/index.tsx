95009d536e3f0e0bd4f043152cdbf8087f11b46e
random776 <kazuki.tdn@gmail.com>
2023-08-04 18:34:05 +0900

    大事な一寸法師無くしたりしたら大変よ

--- src/pages/Test/index.tsx
+++ src/pages/Test/index.tsx
@@ -7,7 +7,7 @@ export default function Result() {
         <Header />
         <div className="sonota">
             <h2 className="color">共通テスト予想問題</h2>
-            <p>共通テスト予想問題（笑）の作成中です。パロディー程度のものなので、共通テスト対策としての実用性は皆無です。期待していた方がいらっしゃったらごめんなさい（いるわけない）。</p>
+            <p>共通テスト予想問題（笑）の作成中です。パロディー程度のものなので、共通テスト対策としての実用性は皆無です。期待していた方がいらっしゃったら謝るけれども、いるわけないので謝らない。</p>
             <ul className="height">
                 <li><a href="https://random776.github.io/kacchan-uts2-22/pdf/butsuri.pdf">物理(105KB)</a></li>
             </ul>
