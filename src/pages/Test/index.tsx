e361de35e5dd3e503ab50d6cea3e87405f383df8
random776 <kazuki.tdn@gmail.com>
2023-08-04 17:20:52 +0900

    delete pdf

--- src/pages/Test/index.tsx
+++ src/pages/Test/index.tsx
@@ -9,7 +9,7 @@ export default function Result() {
             <h2 className="color">共通テスト予想問題</h2>
             <p>共通テスト予想問題（笑）の作成中です。パロディー程度のものなので、共通テスト対策としての実用性は皆無です。期待していた方がいらっしゃったらごめんなさい。</p>
             <ul className="height">
-                <li><a href="https://random776.github.io/kacchan_home/pdf/butsuri.pdf"></a></li>
+                <li><a href="https://random776.github.io/kacchan_uts2_22/pdf/butsuri.pdf"></a></li>
             </ul>
         </div>
         <SubMenu />
